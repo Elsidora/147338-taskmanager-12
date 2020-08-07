2bbf25ed00a65ec78d7bf99848d259b5935ce9e2
ella <ellasidorova@yandex.ru>
2020-08-07 03:28:42 +0300

    show tasks logic

--- src/main.js
+++ src/main.js
@@ -9,8 +9,9 @@ import {createButtonLoadingTemplate} from "./view/button-load.js";
 import {generateTask} from "./mock/task.js";
 import {generateFilter} from "./mock/filter.js";
 
+const TASK_COUNT = 22;
+const TASK_COUNT_PER_STEP = 8;
 
-const TASK_COUNT = 4;
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask); // Метод fill() заполняет все элементы массива от начального до конечного индексов одним значением
 // (в данном случае - undefined). Метод map() создаёт новый массив с результатом вызова указанной функции для каждого элемента массива.
@@ -27,7 +28,19 @@ const renderBoardElement = () => {
     renderHtmlElement(boardTasksElement, createCardTaskTemplate(tasks[0]), `beforeend`);
   }
 
-  renderHtmlElement(boardElement, createButtonLoadingTemplate(), `beforeend`);
+  // renderHtmlElement(boardElement, createButtonLoadingTemplate(), `beforeend`);
+
+  if (tasks.length > TASK_COUNT_PER_STEP) {
+    renderHtmlElement(boardElement, createButtonLoadingTemplate(), `beforeend`);
+
+    const loadMoreButton = boardElement.querySelector(`.load-more`);
+
+    loadMoreButton.addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+      alert('Works!');
+    });
+  }
+
 };
 
 renderHtmlElement(siteMainControlElement, createSiteMenuTemplate(), `beforeend`); // beforeend вставляет последним дочерним элементом контейнера
