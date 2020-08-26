edc094bf556d60e3726666550523ff805411b021
ella <ellasidorova@yandex.ru>
2020-08-26 04:20:17 +0300

    renderTask && loadMoreButton for presenter/board.js

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -7,7 +7,7 @@ import ButtonLoadingView from "../view/button-load";
 import BoardView from "../view/board";
 import TaskListView from "../view/task-list";
 import NoTaskView from "../view/no-task";
-import {renderHTMLElement, RenderPosition} from "../utils/render.js";
+import {renderHTMLElement, RenderPosition, replace, remove, closeElement} from "../utils/render.js";
 
 const TASK_COUNT_PER_STEP = 8;
 
@@ -34,24 +34,81 @@ export default class Board {
 
   _renderSort() {
     // Метод для рендеринга сортировки
+
+    renderHTMLElement(this._boardComponent, this._sortComponent, RenderPosition.AFTERBEGIN);
   }
 
-  _renderTask() {
+  _renderTask(task) {
     // Метод, куда уйдёт логика по созданию и рендерингу компонетов задачи,
     // текущая функция renderTask в main.js
+
+    const taskComponent = new TaskView(task);
+    const taskEditComponent = new TaskEditView(task);
+
+    const replaceCardToForm = () => {
+      replace(taskEditComponent, taskComponent);
+    };
+
+    const replaceFormToCard = () => {
+      replace(taskComponent, taskEditComponent);
+    };
+
+    const closeFormEditTask = () => {
+      replaceFormToCard();
+      document.removeEventListener(`keydown`, onEscapePress);
+    };
+
+    const onEscapePress = (evt) => {
+      closeElement.isEscapeEvent(evt, closeFormEditTask);
+    };
+
+    taskComponent.setEditClickHandler(() => {
+      replaceCardToForm();
+      document.addEventListener(`keydown`, onEscapePress);
+    });
+
+    taskEditComponent.setFormSubmitHandler(() => {
+      closeFormEditTask();
+    });
+
+    renderHTMLElement(this._taskListComponent, taskComponent, RenderPosition.BEFOREEND);
   }
 
   _renderTasks(from, to) {
     // Метод для рендеринга N-задач за раз
+
+    this._boardTasks
+      .slice(from, to)
+      .forEach((boardTask) => this._renderTask(boardTask));
   }
 
   _renderNoTasks() {
     // Метод для рендеринга заглушки
+
+    renderHTMLElement(this._boardComponent, this._noTaskComponent, RenderPosition.AFTERBEGIN);
   }
 
   _renderLoadMoreButton() {
     // Метод, куда уйдёт логика по отрисовке компонетов задачи,
     // текущая функция renderTask в main.js
+
+    let renderedTaskCount = TASK_COUNT_PER_STEP;
+
+    const loadMoreButtonComponent = new ButtonLoadingView();
+
+    renderHTMLElement(this._boardComponent, loadMoreButtonComponent, RenderPosition.BEFOREEND);
+
+    loadMoreButtonComponent.setClickHandler(() => {
+      this._boardTasks
+        .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+        .forEach((boardTask) => this._renderTask(boardTask));
+
+      renderedTaskCount += TASK_COUNT_PER_STEP;
+
+      if (renderedTaskCount >= this._boardTasks.length) {
+        remove(loadMoreButtonComponent);
+      }
+    });
   }
 
   _renderBoard() {
